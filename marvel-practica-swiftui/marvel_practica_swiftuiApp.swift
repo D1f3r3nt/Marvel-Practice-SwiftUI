da29a1ffe1e5b6c07c8e9bc4e3630bf318b9db96
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2023-11-14 20:28:15 +0100

    Initial Commit

---
+++ marvel-practica-swiftui/marvel_practica_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvel_practica_swiftuiApp.swift
+//  marvel-practica-swiftui
+//
+//  Created by Marc Santisteban Ruiz on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct marvel_practica_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
